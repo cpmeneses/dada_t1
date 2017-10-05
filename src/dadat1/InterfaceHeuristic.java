f2e451043ecf97e9a99661f2623316d2810b4f78
carlos meneses <cpmeneses94@gmail.com>
2017-10-05 20:33:49 -0300

    mas heuristic
    mas para heuristic

--- src/dadat1/InterfaceHeuristic.java
+++ src/dadat1/InterfaceHeuristic.java
@@ -1,5 +1,5 @@
 package dadat1;
 
 public interface InterfaceHeuristic {
-  public Node[] splitNodeLeaf(Node node, Rectangle rect, int m, int M); //divide un nodo en dos segun su heuristica.
+  public Node[] splitNodeLeaf(NodeLeaf node, Rectangle rect, int m, int M); //divide un nodo en dos segun su heuristica.
 }
